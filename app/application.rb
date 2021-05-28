a6ef3c36bd31918c3d10ba9e6a683b5b89e91437
Bryce B <bryce.boddy@gmail.com>
2021-05-28 12:09:13 +0000

    Automatically backed up by Learn

--- app/application.rb
+++ app/application.rb
@@ -6,11 +6,11 @@ class Application
     time1 = Time.new
     presp.write time1.inspect
 
-    if num_1==num_2 && num_2==num_3
-      resp.write "You Win"
-    else
-      resp.write "You Lose"
-    end
+    # if num_1==num_2 && num_2==num_3
+    #   resp.write "You Win"
+    # else
+    #   resp.write "You Lose"
+    # end
 
     resp.finish
   end
