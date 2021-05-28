56443534c00c97842db04091111299f19bcb9c23
Bryce B <bryce.boddy@gmail.com>
2021-05-28 12:09:08 +0000

    Automatically backed up by Learn

---
+++ app/application.rb
@@ -0,0 +1,18 @@
+class Application
+
+  def call(env)
+    resp = Rack::Response.new
+    
+    time1 = Time.new
+    presp.write time1.inspect
+    
+    if num_1==num_2 && num_2==num_3
+      resp.write "You Win"
+    else
+      resp.write "You Lose"
+    end
+
+    resp.finish
+  end
+
+end
